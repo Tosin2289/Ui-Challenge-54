bc83a03a182c1e5b7eaf7d9aefb8e9a9de6d32c6
Tosin2289 <ayoolatosin00@gmail.com>
2023-02-25 22:50:47 +0100

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,310 @@
-import 'package:flutter/material.dart';
+import "dart:ui";
+import "package:flutter/material.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 void main() {
   runApp(const MyApp());
 }
 
+SharedPreferences? dhananjay;
+sharedpreferences() async {
+  dhananjay = await SharedPreferences.getInstance();
+  dhananjay!.getInt("onboarding") ?? 0;
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 25,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  const Icon(
+                    Icons.align_horizontal_left_rounded,
+                    size: 25,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 1.8,
+                  ),
+                  const Icon(
+                    Icons.search_rounded,
+                    size: 25,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 50,
+                  ),
+                  const Icon(
+                    Icons.notification_add_outlined,
+                    size: 25,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 50,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.width / 8,
+                    width: MediaQuery.of(context).size.width / 8,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                            image: AssetImage("assets/img.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width / 10,
+              ),
+              Text(
+                " Get Free",
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height / 20,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 60,
+              ),
+              Text(
+                " Consultation Now!",
+                style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height / 20,
+                    fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width / 10,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    height: MediaQuery.of(context).size.width / 6,
+                    width: MediaQuery.of(context).size.width / 6,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.green[600],
+                    ),
+                    child: Image.asset(
+                      "assets/img2.png",
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Personal Loan",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 45,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 70),
+                      Text(
+                        "2.8k Answers",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 60,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3.1,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Colors.black,
+                    size: MediaQuery.of(context).size.width / 20,
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 50),
+              const Divider(),
+              Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    height: MediaQuery.of(context).size.width / 6,
+                    width: MediaQuery.of(context).size.width / 6,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.green[600],
+                    ),
+                    child: Image.asset(
+                      "assets/img3.png",
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Home Loan",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 45,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 70),
+                      Text(
+                        "9.5k Answers",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 60,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2.6,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Colors.black,
+                    size: MediaQuery.of(context).size.width / 20,
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 50),
+              const Divider(),
+              Row(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    height: MediaQuery.of(context).size.width / 6,
+                    width: MediaQuery.of(context).size.width / 6,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.green[600],
+                    ),
+                    child: Image.asset(
+                      "assets/img4.png",
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 25,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Business Loan",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 45,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 70),
+                      Text(
+                        "254 Answers",
+                        style: TextStyle(
+                            fontSize: MediaQuery.of(context).size.height / 60,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3.2,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Colors.black,
+                    size: MediaQuery.of(context).size.width / 20,
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 24.4),
+              Container(
+                height: MediaQuery.of(context).size.height / 5,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.only(
+                      topLeft: containerRadius,
+                      topRight: containerRadius,
+                    )),
+                child: Column(
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height / 24.4),
+                    Text(
+                      "Loan Calculator",
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height / 40,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height / 110),
+                    Text(
+                      "Now Calculate Your Loan EMI With Easy",
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height / 50,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height / 60),
+                    Container(
+                      height: MediaQuery.of(context).size.height / 15,
+                      width: MediaQuery.of(context).size.width / 3,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(25)),
+                      child: Center(
+                        child: Text(
+                          "Check Now",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: MediaQuery.of(context).size.height / 50,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  var containerRadius = Radius.circular(30);
 }
